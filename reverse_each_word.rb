29778d69a9381a60dcb53c4988c3064c1d935fd4
Alexander Marusin <amarusin18@gmail.com>
2020-07-15 06:01:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
 def reverse_each_word(string)
-  array = []
-  array << string.split
-  array.collect do |word|
-    new = []
-    new << word.reverse_each
-    return new.join(" ")
+  string.split.map {|word| word.reverse}.join(" ")
   end
 end
